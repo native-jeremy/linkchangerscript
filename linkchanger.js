6073445efd37c58a99691abc241097eee01118b7
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2022-12-19 15:39:23 +1100

    Update linkchanger.js

--- linkchanger.js
+++ linkchanger.js
@@ -1,5 +1,5 @@
 let cell = document.querySelectorAll('.project-cell')
 
 for (let i = 0; i < cell.length; i++) {
-  cell[i].href = 'https://beardwood-group-website.webflow.io/individual-projects?/slideNum='+cells[i];
+  cell[i].href = 'https://beardwood-group-website.webflow.io/individual-projects?/slideNum='+cell[i];
 }
