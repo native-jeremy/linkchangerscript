830810f5d9190b013b92c48cf3978c2932c77b53
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2022-12-19 16:17:29 +1100

    Update linkchanger.js

--- linkchanger.js
+++ linkchanger.js
@@ -1,6 +1,5 @@
 let cell = document.querySelectorAll('.project-cell')
 
 for (let i = 0; i < cell.length; i++) {
-  let cells = cell[i]
-  cells.href = 'https://beardwood-group-website.webflow.io/individual-projects?/slideNum='+cells;
+  cell[i].href = cell[i];
 }
