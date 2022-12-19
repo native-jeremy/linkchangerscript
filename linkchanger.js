0d30e09f55cdc5e676f9f7649761506829fd9178
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2022-12-19 15:22:30 +1100

    Add files via upload

---
+++ linkchanger.js
@@ -0,0 +1,7 @@
+let cell = document.querySelectorAll('.project-cell')
+
+for (let i = 0; i < cell.length; i++) {
+  const cells = cell[i];
+  
+  cells.href = 'https://beardwood-group-website.webflow.io/individual-projects?/slideNum='
+}
